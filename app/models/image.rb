aadbcd061125f34fb8b7bfa7383160b9199366db
Lei Zhang <lei.zhang@appfolio.com>
2019-09-17 14:37:09 -0700

    lei - add tag [mytags] for Image model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,5 @@
 class Image < ApplicationRecord
+  acts_as_taggable_on :mytags
+
   validates :web_url, url: true
 end
--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -20,4 +20,35 @@ class ImageTest < ActiveSupport::TestCase
     refute_predicate image, :valid?
     assert_equal 'is an invalid URL', image.errors.messages[:web_url].first
   end
+
+  def test_image_tag__null
+    image = Image.new(web_url: 'https://i.pinimg.com/474x/b4/bb/eb/b4bbeb2aaafd59040e56df10ad885b40.jpg')
+
+    assert_predicate image, :valid?
+    assert_equal [], image.mytag_list
+  end
+
+  def test_image_tag__one
+    image = Image.new(web_url: 'https://img.apmcdn.org/5d9c531be5686d2572bcab206df39a230c44f642/uncropped/a85434-20161213-cat.jpg')
+    image.mytag_list.add('cute')
+
+    assert_predicate image, :valid?
+    assert_equal ['cute'], image.mytag_list
+  end
+
+  def test_image_tag__multiple
+    image = Image.new(web_url: 'https://i.pinimg.com/474x/b4/bb/eb/b4bbeb2aaafd59040e56df10ad885b40.jpg')
+    image.mytag_list.add('cute', 'panda')
+
+    assert_predicate image, :valid?
+    assert_equal %w[cute panda], image.mytag_list
+  end
+
+  def test_image_tag__parse
+    image = Image.new(web_url: 'https://images-na.ssl-images-amazon.com/images/I/71kkMXAcLCL._SY355_.png')
+    image.mytag_list.add('cute, panda, real', parse: true)
+
+    assert_predicate image, :valid?
+    assert_equal %w[cute panda real], image.mytag_list
+  end
 end
