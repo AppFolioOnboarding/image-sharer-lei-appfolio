cb84455640721850f30d5a266ee3341f5850891d
Lei Zhang <lei.zhang@appfolio.com>
2019-09-24 11:13:53 -0700

    lei - add Footer

--- app/javascript/image_sharing/components/Footer.js
+++ app/javascript/image_sharing/components/Footer.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 
 class Footer extends Component {
-  /* Implement your Footer component here */
+  render() {
+    return <div id="footer">Copyright: Appfolio Inc. Onboarding</div>;
+  }
 }
 
 export default Footer;
