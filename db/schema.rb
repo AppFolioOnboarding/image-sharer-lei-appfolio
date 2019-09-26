d75d7b21adeb84b062c6955f25266553a86179a6
Lei Zhang <lei.zhang@appfolio.com>
2019-09-26 10:44:31 -0700

    lei - add model Feedback

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_17_005726) do
+ActiveRecord::Schema.define(version: 2019_09_26_064608) do
+
+  create_table "feedbacks", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "images", force: :cascade do |t|
     t.text "web_url"
